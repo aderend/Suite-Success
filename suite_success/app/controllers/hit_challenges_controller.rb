055aed73f69f7781a5d0f77c91de7718e6b1cce8
aderend <d.dogrusoy@gmail.com>
2016-05-13 12:34:57 -0400

    anle to create hit challenge froma game, show the hit challenge page

---
+++ suite_success/app/controllers/hit_challenges_controller.rb
@@ -0,0 +1,18 @@
+class HitChallengesController < ApplicationController
+
+  def create
+    game = Game.find(params[:id])
+    hit_challenge = HitChallenge.new(game_id: game.id)
+    if hit_challenge.save
+      redirect_to show_hit_challenge_path(hit_challenge)
+    else
+      redirect_to games_show_path(game)
+    end
+  end
+
+  def show
+    hit_challenge = HitChallenge.find(params[:id])
+    @home_players = hit_challenge.game.home_players
+  end
+
+end
