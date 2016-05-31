8f4f99d26c3f7131b9226480fa3b84e46be8ab96
aderend <d.dogrusoy@gmail.com>
2016-05-31 12:25:06 -0400

    change redirect on session create

--- suite_success/app/controllers/sessions_controller.rb
+++ suite_success/app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user
       session[:user_id] = user.id
-      redirect_to root_path
-    else
       redirect_to user_path(user)
+    else
+      redirect_to root_path
     end
   end
 
