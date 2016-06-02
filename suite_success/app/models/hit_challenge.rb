bce709da69c34b98d05b5da947d8c282b1685671
aderend <d.dogrusoy@gmail.com>
2016-06-02 14:54:52 -0400

    WIP add methods to calculate points

--- suite_success/app/models/hit_challenge.rb
+++ suite_success/app/models/hit_challenge.rb
@@ -12,4 +12,30 @@ class HitChallenge < ActiveRecord::Base
     game.home_players.where(position: pos)
   end
 
+  def calculate_bats
+    home_players = self.game.home_players
+    away_players = self.game.away_players
+    total_at_bat = 0
+    away_players.each do |p|
+      total_at_bat += p.at_bat
+    end
+    home_players.each do |p|
+      total_at_bat += p.at_bat
+    end
+    total_at_bat
+  end
+
+  def list_player_names
+    game_players = []
+    self.game.home_players.each do |p|
+      game_players << p.name.downcase.delete(" ")
+    end
+    self.game.away_players.each do |p|
+      game_players << p.name.downcase.delete(" ")
+    end
+    game_players
+  end
+
+
+
 end
--- suite_success/app/models/hit_guess.rb
+++ suite_success/app/models/hit_guess.rb
@@ -1,4 +1,23 @@
 class HitGuess < ActiveRecord::Base
   belongs_to :hit_challenge
   belongs_to :user
+
+  HIT_GUESS_POSITIONS = ["pos_c", "pos_1b", "pos_2b", "pos_3b", "pos_ss", "pos_of1", "pos_of2", "pos_of3", "pos_dh", "away_pos_c", "away_pos_1b", "away_pos_2b", "away_pos_3b", "away_pos_ss", "away_pos_of1", "away_pos_of2", "away_pos_of3", "away_pos_dh"]
+
+  def list_players
+    players = []
+    HitGuess::HIT_GUESS_POSITIONS.each do |pos|
+      players << self[pos].gsub(/[^a-zA-Z]/, '').downcase unless self[pos] == nil
+    end
+    players.delete_if {|p| p == "" }
+    players
+  end
+
+  def calculate_points
+    duplicate_players = self.hit_challenge.list_player_names + list_players
+
+
+  end
+
+
 end
