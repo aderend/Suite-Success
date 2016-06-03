0eb276a4c2aa271d1ca9eabbaf222f761456d682
aderend <d.dogrusoy@gmail.com>
2016-06-03 11:06:40 -0400

    auto schema change?

--- suite_success/db/schema.rb
+++ suite_success/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531162742) do
+ActiveRecord::Schema.define(version: 20160602163711) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "body"
@@ -82,6 +82,11 @@ ActiveRecord::Schema.define(version: 20160531162742) do
     t.integer  "user_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "hit_results", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "hit_stats", force: :cascade do |t|
