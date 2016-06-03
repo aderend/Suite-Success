c56e03e7c854be807e437713f38eaafb031ceb0b
aderend <d.dogrusoy@gmail.com>
2016-06-03 11:49:20 -0400

    fix navbar, change sqlite to postgresql

--- suite_success/db/schema.rb
+++ suite_success/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160602163711) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string   "body"
     t.integer  "question_id"
