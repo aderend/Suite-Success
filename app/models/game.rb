22f510a32653e725a1c39275f3305daafad2855a
aderend <d.dogrusoy@gmail.com>
2016-06-08 13:56:08 -0400

    move total at_bats method to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,17 @@ class Game < ActiveRecord::Base
   has_many :away_players
   has_many :home_players
 
+   def total_hits
+    home_players = self.home_players
+    away_players = self.away_players
+    total_at_bat = 0
+    away_players.each do |p|
+      total_at_bat += p.at_bat
+    end
+    home_players.each do |p|
+      total_at_bat += p.at_bat
+    end
+    total_at_bat
+  end
+
 end
