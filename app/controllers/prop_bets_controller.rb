dabb0e234f4060644982ed80a7dcb589e0ff5ba2
aderend <d.dogrusoy@gmail.com>
2016-06-10 13:38:22 -0400

    create and new methods

--- app/controllers/prop_bets_controller.rb
+++ app/controllers/prop_bets_controller.rb
@@ -1,9 +1,19 @@
 class PropBetsController < ApplicationController
+
   def new
+    # byebug
     @prop_bet = PropBet.new
   end
 
   def create
-
+    # byebug
+    game = Game.find(params[:prop_bet][:game_id])
+    prop_bet = PropBet.new(game_id: game.id, suite_num: params[:prop_bet][:suite_num])
+    if prop_bet.save
+      redirect_to new_question_path
+    else
+      redirect_to games_show_path(game)
+    end
   end
+
 end
