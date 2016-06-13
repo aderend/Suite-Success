e02169927b4e427b714b030b19343d21e6a28ed6
aderend <d.dogrusoy@gmail.com>
2016-06-13 15:21:35 -0400

    WIP submitting multiple answers at once

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,24 @@
 class AnswersController < ApplicationController
+
+  def create
+    prop_bet = PropBet.find(params[:id])
+    user = User.find(session[:user_id])
+    @num_of_questions = prop_bet.questions.count
+    byebug
+    @answers = num_of_questions.times do
+
+    end
+    if @answers.save
+      redirect_to game_show_path(prop_bet.game)
+    else
+      render show_prop_bet_path(prop_bet)
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:choice, :question_id)
+  end
+
 end
