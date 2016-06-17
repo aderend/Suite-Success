c55e6ceb049577846b247b9c24912f421b3f8a95
aderend <d.dogrusoy@gmail.com>
2016-06-17 14:57:35 -0400

    additions to seed and small changes to migrations

--- db/migrate/20160504162614_create_answers.rb
+++ db/migrate/20160504162614_create_answers.rb
@@ -2,10 +2,13 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.string :choice
-      t.integer :prop_bet_id
       t.integer :user_id
+      t.integer :question_id
+      t.integer :prop_bet_id
 
       t.timestamps null: false
     end
   end
+
+
 end
