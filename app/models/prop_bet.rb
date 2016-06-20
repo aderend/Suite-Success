7f1fa29be942218c913e3e038865211724ff8813
aderend <d.dogrusoy@gmail.com>
2016-06-20 13:22:54 -0400

    display scores method

--- app/models/prop_bet.rb
+++ app/models/prop_bet.rb
@@ -5,32 +5,12 @@ class PropBet < ActiveRecord::Base
   has_many :users
   has_many :answers
 
-  def create_submissions_hash
-    results = {}
-    self.questions.each do |q|
-      q.answers.each do |ans|
-        results[ans.user] = ans.create_answer_hash[ans.user]
-      end
+  def display_scores
+    scores_hash = {}
+    self.suite.users.each do |u|
+      scores_hash[u.name] = u.calculate_prop_score(self)
     end
-    results
-  end
-
-  def create_scores_hash
-    hash = {}
-    self.suite.users.each do |user|
-      hash[user] = 0
-    end
-    hash
-  end
-
-  def calculate_score(submissions, score_hash)
-    scores = {score_hash}
-    submissions.each do |sub|
-      if sub[2] == sub[1]
-        scores[sub[0].name] += 1
-      end
-    end
-    scores
+    scores_hash
   end
 
 end
