5506d3cd46064656d3663ef1377045337a7f623a
aderend <d.dogrusoy@gmail.com>
2016-06-20 13:19:49 -0400

    remove unused method

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,24 +6,14 @@ class Answer < ActiveRecord::Base
   def self.submit_many_answers(choices, user, questions)
     unsaved_answers = []
     choices.each_with_index do |cho, i|
-      unsaved_answers << Answer.new(choice: cho, user_id: user.id, question: questions[i])
+      unsaved_answers << Answer.new(choice: cho, user_id: user.id, question: questions[i], prop_bet: questions[i].prop_bet)
       i += 1
     end
     unsaved_answers
-  end
+  en
 
   def self.check_save(answer_array)
     return true if answer_array.each {|ans| ans.save}
   end
-
-  def create_answer_hash
-    hash = {}
-    prop_bet = self.question.prop_bet
-    prop_bet.questions.each do |q|
-      hash[self.user] = [q, q.correct_choice, self.choice]
-    end
-    hash
-  end
-
 
 end
