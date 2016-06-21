2a8e4a881e99efa2e28a563b7a3bec74f4044e65
aderend <d.dogrusoy@gmail.com>
2016-06-21 15:51:54 -0400

    change game show to show prop bets

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       game = Game.last
       suite = Suite.find_by(suite_number: params[:session][:suite_id], game_id: game.id)
-
       hit_challenge = HitChallenge.find_by(game_id: game.id, suite_id: suite.id)
       if ((hit_challenge == nil) || (suite.suite_number== "0")) || is_admin
         redirect_to user_path(user)
       else
+        user.suite_id = suite.id
         redirect_to play_hit_challenge_path(hit_challenge)
       end
     else
