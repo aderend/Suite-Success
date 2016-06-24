916ba555e87e2c75c731763f23399b6496c29f73
aderend <d.dogrusoy@gmail.com>
2016-06-24 14:00:04 -0400

    fix tables with teams for players, and remove away pos from hit guess

--- db/migrate/20160509162242_create_home_players.rb
+++ db/migrate/20160509162242_create_home_players.rb
@@ -5,6 +5,7 @@ class CreateHomePlayers < ActiveRecord::Migration
       t.string :position, null: false
       t.string :batting_avg, null: false
       t.integer :at_bat, default: 0
+      t.string :team
       t.integer :game_id, null: false
 
       t.timestamps null: false
