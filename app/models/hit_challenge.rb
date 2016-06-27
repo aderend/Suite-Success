37a306a3b15cd3c395c704a775fc32de3dcc649d
aderend <d.dogrusoy@gmail.com>
2016-06-27 12:45:38 -0400

    display scores method

--- app/models/hit_challenge.rb
+++ app/models/hit_challenge.rb
@@ -5,4 +5,12 @@ class HitChallenge < ActiveRecord::Base
   has_many :answers
   has_many :users
 
+  def display_scores
+    scores = {}
+    self.hit_guesses.each do |guess|
+      scores[guess.calculate_points] = [guess.user.name, guess.total_hits]
+    end
+    scores.sort_by {|key, value| key}.reverse.to_h
+  end
+
 end
